f48a138b975453f3863a211145da001831ac9db2
DOLUWAMU-TAIWO <91460092+DOLUWAMU-TAIWO@users.noreply.github.com>
2023-05-25 00:16:02 +0200

    Update 8 BIT REGISTER.vhd

--- HARDWARE_Engineering/Lab 3/D-flipflop and 8bit register/8 BIT REGISTER.vhd
+++ HARDWARE_Engineering/Lab 3/D-flipflop and 8bit register/8 BIT REGISTER.vhd
@@ -1,24 +1,3 @@
-entity DFlipFlop is
-  port (
-    D : in bit;
-    CLK : in bit;
-    Q : out bit;
-    QN : out bit
-  );
-end entity DFlipFlop;
-
-architecture Behavioral of DFlipFlop is
-begin
-  process (CLK)
-  begin
-    if rising_edge(CLK) then
-      Q <= D;
-      QN <= not D;
-    end if;
-  end process;
-end architecture Behavioral;
-
-
 entity ShiftRegister is
   port (
     D : in bit;
